c5dc004e11166af50b69b123d13e2898402291bd
Frankie <frankie@FrankiedeMacBook-Pro.local>
2026-09-08 23:28:44 +0800

    Clarify cloud build template guidance

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 const account = () => import("./account.js?v=1efd29f8c34c0b77");
 const collaboration = () => import("./collaboration.js?v=b5593ce58e7ebae4");
-const platform = () => import("./manage.js?v=eb6bcc9d0f87c84c");
+const platform = () => import("./manage.js?v=5b7f6291703af45c");
 import {
   keyPage,
   forgePage,
